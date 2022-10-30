067a4b0d4b2058d441896d63210b1100aa7e39a2
Vltemadev <cfpro.tema@mail.ru>
2022-10-30 12:56:21 +0600

    static files changed

--- travelProject/travelProject/settings.py
+++ travelProject/travelProject/settings.py
@@ -125,8 +125,9 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+
     )
